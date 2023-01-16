5821f676736e38e4388335e93f2153a664af241f
lomick2090 <lomick2090@gmail.com>
2023-01-16 17:02:15 -0600

    Add unworking function KEKW

--- src/index.js
+++ src/index.js
@@ -2,23 +2,48 @@ const submitButton = document.querySelector('button');
 const searchInput = document.getElementById('city');
 
 async function getSearchData(city) {
-    let data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=b5a14a7cc940d71738c874058413abb4`)
+    let data = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=b5a14a7cc940d71738c874058413abb4`)
         .then(response => {
             return response.json();
         })
 
-    console.log(data);
+    return data;
 }
 
-submitButton.addEventListener('click', () => getSearchData(searchInput.value));
+function parseData(data) {
+    let dataObj = [];
+    for (let i = 2; i <= 8; i+2) {
+        let dayArray = [];
+        let day = {};
+        day.time = data.list[i].dt_txt;
+        day.tempHigh = data.list[i].main.temp_max;
+        day.tempLow = data.list[i].main.temp_min;
+        day.weather = data.list[i].weather[0].main;
+        day.weatherDesc = data.list[i].weather[0].description;
+
+        dayArray.push(day);
+
+        if (i = 8) {
+            dataObj.push(dayArray);
+        }
+    }
+
+    console.log(dataObj);
+}
+
+submitButton.addEventListener('click', async function() { 
+    let data = await getSearchData(searchInput.value)
+    parseData(data)
+    });
 
 
 
 
-fetch('https://api.openweathermap.org/data/2.5/weather?q=chicago&appid=b5a14a7cc940d71738c874058413abb4')
+fetch('https://api.openweathermap.org/data/2.5/forecast?q=chicago&units=imperial&appid=b5a14a7cc940d71738c874058413abb4')
     .then(response => {
         return response.json()
     })
     .then(data => {
         console.log(data);
+        console.log(data.list[0])
     });
