91ceee52dbe3ffcc0567310e7a77dcb2ce705b63
lomick2090 <lomick2090@gmail.com>
2023-01-16 15:25:42 -0600

    Add search functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+const submitButton = document.querySelector('button');
+const searchInput = document.getElementById('city');
+
+async function getSearchData(city) {
+    let data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=b5a14a7cc940d71738c874058413abb4`)
+        .then(response => {
+            return response.json();
+        })
+
+    console.log(data);
+}
+
+submitButton.addEventListener('click', () => getSearchData(searchInput.value));
+
+
+
+
 fetch('https://api.openweathermap.org/data/2.5/weather?q=chicago&appid=b5a14a7cc940d71738c874058413abb4')
     .then(response => {
         return response.json()
